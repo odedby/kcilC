194d656633be2aacf919d313ee1a594a4aaebc3c
Michael Svit <michael.svit@Michaels-Mac.local>
2018-08-06 15:00:03 +0300

    fix code style

--- src/scraper.js
+++ src/scraper.js
@@ -1,43 +1,42 @@
-const fs = require('fs')
-const dbFunctions = require('./db_functions')
-const parseString = require('xml2js').parseString
+const fs = require('fs');
+const parseString = require('xml2js').parseString;
 
 module.exports.parseXml = (src) => {
-    return new Promise((fulfill, reject) => {
-        fs.readFile(src, (ex, res) => {
-            if (ex) {
-                reject(ex)
-            } else {
-                parseString(res, (err, result) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        fulfill(result.viewentries.viewentry)
-                    }
-                })
-            }
-        })
-    })
-}
+	return new Promise((fulfill, reject) => {
+		fs.readFile(src, (ex, res) => {
+			if (ex) {
+				reject(ex);
+			} else {
+				parseString(res, (err, result) => {
+					if (err) {
+						reject(err);
+					} else {
+						fulfill(result.viewentries.viewentry);
+					}
+				});
+			}
+		});
+	});
+};
 
 module.exports.formatJson = (json) => {
-    return new Promise((fulfill, reject) => {
-        res = []
-        for(var entry of json) {
-            res.push(extractPerson(entry))
-        }
-        fulfill(res)
-    })
-}
+	return new Promise((fulfill, reject) => {
+		res = [];
+		for (let entry of json) {
+			res.push(extractPerson(entry));
+		}
+		fulfill(res);
+	});
+};
 
 function extractPerson(entry) {
-    var data = entry.entrydata
-    return {
-        'fullName': data[0].text[0],
-        'title': data[3].text[0],
-        'country': data[2].text[0],
-        'ext': data[4].text[0],
-        'phone': data[5].text[0],
-        'phoneMobile': data[6].text[0]
-    }
+	let data = entry.entrydata;
+	return {
+		fullName: data[0].text[0],
+		title: data[3].text[0],
+		country: data[2].text[0],
+		ext: data[4].text[0],
+		phone: data[5].text[0],
+		phoneMobile: data[6].text[0],
+	};
 }
