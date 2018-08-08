9c2323b8ac647bbcb63f2bef62e5d840cc7b0de8
Michael Svit <michael.svit@Michaels-Mac.local>
2018-08-08 10:32:33 +0300

    initialize server asynchronously with a promise, wait for db to be ready

--- src/app.js
+++ src/app.js
@@ -11,59 +11,67 @@ const db = new Redis(PORT, HOST, SRC, scraper);
 
 const sendErr = () => res.status(500).send('Internal server error');
 
-app.use(express.json());
+module.exports = () => {
+	return db.init().then(configApp);
+};
 
-app.post('/getPersonByExt', (req, res) => {
-	let json = req.body;
-	db.getPersonByExt(json.ext, json.country).then(
-		(result) => {
-			if (result) {
-				res.json(result);
-			} else {
-				res.status(404).json('User not found');
-			}
-		},
-		(ex) => {
-			console.log('Failed to retrieve person by extention: ' + ex);
-			sendErr(res);
-		}
-	);
-});
+const configApp = () => {
+	return new Promise((fulfill, reject) => {
+		app.use(express.json());
 
-app.get('/getPersons', (req, res) => {
-	db.getPersons().then(
-		(result) => res.json(result),
-		(ex) => {
-			console.log('Failed to retrieve person: ' + ex);
-			sendErr(res);
-		}
-	);
-});
+		app.post('/getPersonByExt', (req, res) => {
+			let json = req.body;
+			db.getPersonByExt(json.ext, json.country).then(
+				(result) => {
+					if (result) {
+						res.json(result);
+					} else {
+						res.status(404).json('User not found');
+					}
+				},
+				(ex) => {
+					console.log('Failed to retrieve person by extention: ' + ex);
+					sendErr(res);
+				}
+			);
+		});
 
-app.get('/persons/:fullName', (req, res) => {
-	db.getPersonByName(req.params.fullName).then(
-		(result) => {
-			if (result) {
-				res.json(result);
-			} else {
-				res.status(404).json('User not found');
-			}
-		},
-		(ex) => {
-			console.log('Failed to retrieve person by name: ' + ex);
-			sendErr(res);
-		}
-	);
-});
+		app.get('/getPersons', (req, res) => {
+			db.getPersons().then(
+				(result) => res.json(result),
+				(ex) => {
+					console.log('Failed to retrieve person: ' + ex);
+					sendErr(res);
+				}
+			);
+		});
 
-app.get('/stats', (req, res) => {
-	db.getStats().then(
-		(result) => res.json(result),
-		(ex) => {
-			console.log('Failed to retrieve stats');
-			sendErr(res);
-		}
-	);
-});
+		app.get('/persons/:fullName', (req, res) => {
+			db.getPersonByName(req.params.fullName).then(
+				(result) => {
+					if (result) {
+						res.json(result);
+					} else {
+						res.status(404).json('User not found');
+					}
+				},
+				(ex) => {
+					console.log('Failed to retrieve person by name: ' + ex);
+					sendErr(res);
+				}
+			);
+		});
 
-module.exports = app;
+		app.get('/stats', (req, res) => {
+			db.getStats().then(
+				(result) => res.json(result),
+				(ex) => {
+					console.log('Failed to retrieve stats');
+					sendErr(res);
+				}
+			);
+		});
+
+		fulfill(app);
+	});
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
 const PORT = 3000;
 
-app.listen(PORT, () => console.log('Caller ID app listening on port 3000'));
+app().then((server) =>
+	server.listen(PORT, () => console.log('kcilC app listening on port 3000'))
+);
